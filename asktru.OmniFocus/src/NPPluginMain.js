b5431d03e4f7e79a446e830f063dfd211d05ceb6
Anton Skliar <asklyar@air.io>
2024-01-14 21:33:52 -0500

    Basic version of the plugin - creates a task in OmniFocus inbox with note title and link to the note - creates subtasks in OmniFocus that reflect tasks mentioned in the note as well as their due dates
    For now it does not parse hashtags and mentions, as well as indented content that could potentially be inserted as a note text.

--- asktru.OmniFocus/src/NPPluginMain.js
+++ asktru.OmniFocus/src/NPPluginMain.js
@@ -7,7 +7,38 @@ import { createRunPluginCallbackUrl } from '@helpers/general'
 // eslint-disable-next-line require-await
 export async function sendToOmniFocus(): Promise<void> {
     
-    console.log('Sending to OmniFocus is not implemented yet');
+    console.log('Sending to OmniFocus started...');
+    
+    let noteTitle = Editor.title;
+    let noteUrl = 'noteplan://x-callback-url/openNote?noteTitle=' + encodeURIComponent(noteTitle);
+    let omniFocusContent = '- ' + noteTitle + '\n\t[Open in NotePlan](' + noteUrl + ')';
+    
+    let noteLines = Editor.paragraphs;
+    console.log(noteLines.length);
+    noteLines.forEach(function (paragraph) {
+        console.log('paragraph processing');
+        console.log(paragraph.type);
+        console.log(paragraph.indents);
+        
+        if (paragraph.type != 'open') return;
+        
+        let prefix = '\t';
+        for (let i = 0; i <= paragraph.indents; i++) prefix += '\t';
+        omniFocusContent += '\n' + prefix + '- ';
+        
+        let taskName = paragraph.content;
+        if (paragraph.date) {
+            taskName = taskName.replace(/>\d{4}\-\d{2}\-\d{2}/, '');
+            taskName += ' @due(' + paragraph.date.toISOString().slice(0, 10) + ')';
+        }
+        omniFocusContent += taskName;
+    });
+    
+    console.log(omniFocusContent)
+    let omniFocusUrl = 'omnifocus://x-callback-url/paste?content=' + encodeURIComponent(omniFocusContent);
+    
+    NotePlan.openURL(omniFocusUrl);
+    
+    console.log('Sending to OmniFocus finished.');
 
 }
-
